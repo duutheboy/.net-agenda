0f49752d2b3cb9e5c03c3222d94d25ab155effe3
duutheboy <eduardobezerra777@outlook.com>
2024-04-21 00:52:12 -0300

    feat: added frontend for the contatos and home

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SiteEmMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
