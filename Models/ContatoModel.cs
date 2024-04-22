dcec8e3e1ac5f0a32af2ffd20ae05b422573ed0b
duutheboy <eduardobezerra777@outlook.com>
2024-04-21 22:57:23 -0300

    feat: added ContatoModel to manage contact data model

---
+++ Models/ContatoModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SiteEmMVC.Models
+{
+    [Table("Contato")]
+    public class ContatoModel
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Column("Email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Column("Telefone")]
+        [Display(Name = "Telefone")]
+        public int Telefone { get; set; }
+    }
+}
